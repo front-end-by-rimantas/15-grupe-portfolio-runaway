752a54eaff334a49b08447155500c54673fbcd27
Greta Sabaliauskienė <sabaliauskiene@yahoo.com>
2020-05-25 16:53:39 +0300

    blogItem js starting point

--- js/blog-section-js/blog-data.js
+++ js/blog-section-js/blog-data.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const blogItem = [
+{
+    commentsCount: 23,
+    picture: "./img/blog_1.png",
+    heading: "Convallis pulvinar morbi. Aenean nisi vitae metus nonummy a morbi.",
+    p,
+    byWho,
+    date: "20 Jan 2019",
+},
+{
+    commentsCount: 18,
+    picture: "./img/blog_2.png",
+    heading: "Ornare fames imperdiet sapien. Iaculis dictum aptent commodo at iaculis.",
+    p,
+    byWho,
+    date: "18 Jan 2019",
+},
+ {
+    commentsCount: 23,
+    picture: "./img/blog_3.png",
+    heading: "Vulputate donec sem purus litora varius auctor augue suscipit hac..",
+    p,
+    byWho,
+    date: "17 Jan 2019",
+},
+
+
+]
