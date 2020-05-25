0fd93e7b80a8d9125cda201cf3681be576873c64
MonikaGu <63239253+MonikaGu@users.noreply.github.com>
2020-05-25 15:20:06 +0300

    Progress bar scroll position detection

--- js/page-home.js
+++ js/page-home.js
@@ -1,13 +1,26 @@
 "use strict";
 
 import skills from './data/skills.js';
-import experience from './data/experience.js';
+// import experience from './data/experience.js';
 
 import renderProgressBar from './components/progress-bar.js';
 
-console.log('Home page..');
-// console.log( experience );
-// console.log( skills );
+const progressBarsSelector = '#skills .skills-data-list';
+renderProgressBar( progressBarsSelector, skills );
 
-renderProgressBar( skills );
+const progressBars = document.querySelectorAll( progressBarsSelector + ' > .progress-bar');
 
+
+window.addEventListener('scroll', () => {
+    const windowBottomlHeight = window.scrollY + window.innerHeight;
+
+    for (let i=0; i<progressBars.length; i++) {
+    
+        const pg = progressBars[i];
+        const pgBottomHeight = pg.offsetTop + pg.offsetHeight;
+        
+        if ( pgBottomHeight < windowBottomlHeight ) {
+            pg.dataset.animated = 'true';
+        } 
+    }
+});
