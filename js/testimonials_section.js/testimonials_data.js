4aa3711de215d56614feff6c703e75ff0b0592e0
MonikaGu <63239253+MonikaGu@users.noreply.github.com>
2020-05-28 00:04:36 +0300

    Create testimonials_data.js

---
+++ js/testimonials_section.js/testimonials_data.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        author: 'Lucas Noah',
+        stars: 5,
+        text: 'Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris.'
+    },
+    {
+        author: 'Grace Ruby',
+        stars: 5,
+        text: 'Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris.'
+    },
+    {
+        author: 'Aisha Lexi',
+        stars: 3.5,
+        text: 'Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris.'
+    },
+    {
+        author: 'Kiara Paige',
+        stars: 4,
+        text: 'Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris.'
+    },
+];
