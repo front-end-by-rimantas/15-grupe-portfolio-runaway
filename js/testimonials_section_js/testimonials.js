31386eefd47e5c0258eb52601dfd1f8c62f26ad9
MonikaGu <63239253+MonikaGu@users.noreply.github.com>
2020-06-01 13:13:18 +0300

    not finished js..

--- js/testimonials_section_js/testimonials.js
+++ js/testimonials_section_js/testimonials.js
@@ -3,27 +3,62 @@
 function renderTestimonials(target, data) {
     const DOM = document.querySelector(target);
 
-    let HTML = '';
+    let testimonialsHTML = '';
     // for ( let i=0; i<data.length; i++ ) {
-    //     HTML += generateTestimonial(data[i]);
+    //     testimonialsHTML += generateTestimonial(data[i]);
     // }
 
-    const random = Math.floor (Math.random() * data.length);
-    HTML = generateTestimonial(data[random]);
+    const middleIndex = Math.floor(data.length / 2);
+    testimonialsHTML = generateTestimonial(data[ middleIndex ]);
+
+    const HTML = `<div class="testimonials" data-index="${middleIndex}">
+                    <div class="list">${testimonialsHTML}</div>
+                    <div class="controls">
+                        <button class="btn previous" type="button">
+                            <i class="fa fa-angle-left"></i>
+                        </button>
+                        <button class="btn next" type="button">
+                            <i class="fa fa-angle-right"></i>
+                        </button>
+                    </div>
+                </div>`;
 
     DOM.innerHTML = HTML;
+
+   const arrows = DOM.querySelectorAll('.controls > .btn');
+
+   arrows.forEach(arrow => arrow.addEventListener('click', updateTestimonials));
+   
     return;
 }
 
 function generateTestimonial(data) {
-    let HTML = `<div class="testimonial">
-                    <div class="picture"><img src="${data.picture}"></div>
-                    <div class="stars">${data.stars}</div>
-                    <div class="author">${data.author}</div>
-                    <div class="text">${data.text}</div>
-                </div>`;
+    const fullStars = Math.round(data.stars * 2) / 2;
 
-    return HTML;
+    const fullHTML = '<i class="fa fa-star"></i>'.repeat( Math.floor(fullStars) );
+    const halfHTML = '<i class="fa fa-star-half-o"></i>'.repeat( fullStars%1 === 0 ? 0 : 1 );
+    const emptyHTML = '<i class="fa fa-star-o"></i>'.repeat( 5 - Math.ceil(fullStars) );
+
+    return `<div class="testimonial">
+                        <div class="picture"><img src="${data.picture}"></div>
+                        <div class="stars">${fullHTML + halfHTML + emptyHTML}</div>
+                        <div class="author">${data.author}</div>
+                        <div class="text paragraph">${data.text}</div>
+                </div>`;
+}
+
+function updateTestimonials( event ) {
+    const element = event.target;
+    const parent = element.closest('.testimonials')
+    let direction = 1;
+    if ( element.classList.contains('previous') ) {
+        direction = -1;
+    }
+    
+    const currentIndex = parseInt(parent.dataset.index);
+    let nextIndex = currentIndex + direction;
+    console.log(nextIndex);
+    
 }
 
 export default renderTestimonials;
--- js/testimonials_section_js/testimonials_data.js
+++ js/testimonials_section_js/testimonials_data.js
@@ -13,13 +13,13 @@ export default [
     },
     {
         picture: './img/testimonial/aisha_png.png',
-        stars: 3.5,
+        stars: 4.5,
         author: 'Aisha Lexi',
         text: 'Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris.'
     },
     {
         picture: './img/testimonial/kiara_png.png',
-        stars: 4,
+        stars: 4.6,
         author: 'Kiara Paige',
         text: 'Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris.'
     },
